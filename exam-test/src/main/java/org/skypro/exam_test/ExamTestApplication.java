c807d92cf9b1c3f8257dc85a12b80728a39d4578
Nikolay <nikshowsix@gmail.com>
2025-04-27 19:30:05 +0300

    Initial

---
+++ exam-test/src/main/java/org/skypro/exam_test/ExamTestApplication.java
@@ -0,0 +1,12 @@
+package org.skypro.exam_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamTestApplication.class, args);
+    }
+}
---
+++ exam-test/src/test/java/org/skypro/exam_test/ExamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.exam_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
