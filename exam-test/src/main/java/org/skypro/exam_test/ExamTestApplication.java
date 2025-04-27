6da7b999bd171f82e154dc8937d01497da595a86
Nikolay <nikshowsix@gmail.com>
2025-04-27 19:55:36 +0300

    Initial

--- exam-test/src/main/java/org/skypro/exam_test/ExamTestApplication.java
+++ exam-test/src/main/java/org/skypro/exam_test/ExamTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamTestApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ExamTestApplication.class, args);
     }
